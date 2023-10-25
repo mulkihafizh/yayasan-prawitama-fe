08683085505cfa242b6f8f4ec46bc3e0a6492bb1
MulkiHafizh <ukiyo142@gmail.com>
2023-10-25 11:24:40 +0700

    fix

--- app/components/Keuangan/Pegawai-Keuangan/chart/page.tsx
+++ app/components/Keuangan/Pegawai-Keuangan/chart/page.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Line } from "react-chartjs-2";
-import {Chart as ChartJS} from 'chart.js/auto'
+"use client";
 
-function LineChart({chartData}: {chartData: any}) {
-    return <Line data={chartData} />;
+import React from "react";
+import { Line } from "react-chartjs-2";
+import { Chart as ChartJS } from "chart.js/auto";
+
+function LineChart({ chartData }: { chartData: any }) {
+  return <Line data={chartData} />;
 }
 
 export default LineChart;
-
