13f09b10e880f0958f53076700147a1f42951ef6
MulkiHafizh <ukiyo142@gmail.com>
2023-10-30 09:30:54 +0700

    deployment fix

--- app/Dashboard-Keuangan/page.tsx
+++ app/Dashboard-Keuangan/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar-Dashboard/page";
 import Header from "../components/Keuangan/Header-Keuangan/page";
-import JumlahPegawai from "../components/Keuangan/Pegawai-Keuangan/page"
-import CardGaji from "../components/Keuangan/CardGaji/page"
-import ListGaji from "../components/Keuangan/ListGaji/page"
+import JumlahPegawai from "../components/Keuangan/Pegawai-Keuangan/page";
+import CardGaji from "../components/Keuangan/CardGaji/page";
+import ListGaji from "../components/Keuangan/ListGaji/page";
 
 export default function page() {
   return (
