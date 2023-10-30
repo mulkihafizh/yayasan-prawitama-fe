10d2b1adc8b8e6977f99b3cea447a4f3b365fb65
MulkiHafizh <ukiyo142@gmail.com>
2023-10-30 10:29:18 +0700

    deployment fix

--- app/components/Kepegawaian/Pegawai-Kepegawaian/chart/page.tsx
+++ app/components/Kepegawaian/Pegawai-Kepegawaian/chart/page.tsx
@@ -1,12 +1,31 @@
 "use client";
 
-import React from 'react'
+import React from "react";
 import { Line } from "react-chartjs-2";
-import {Chart as ChartJS} from 'chart.js/auto'
+import { Chart as ChartJS } from "chart.js/auto";
+import UserData from "@/data/pegawai/page";
 
-function LineChart({chartData}: {chartData: any}) {
-    return <Line data={chartData} />;
+function LineChart({ chartData }: { chartData: any }) {
+  const userData = {
+    labels: UserData.map((data) => data.month),
+    datasets: [
+      {
+        label: "Pegawai Tetap",
+        data: UserData.map((data) => data.PT),
+        tension: 0.3,
+        fill: true,
+        backgroundColor: "rgba(252, 214, 114, 0.8)",
+      },
+      {
+        label: "Pegawai Tidak Tetap",
+        data: UserData.map((data) => data.PTT),
+        tension: 0.3,
+        fill: true,
+        backgroundColor: "rgba(217, 157, 0, 0.9)",
+      },
+    ],
+  };
+  return <Line data={userData} />;
 }
 
 export default LineChart;
-
--- app/components/Keuangan/Pegawai-Keuangan/chart/page.tsx
+++ app/components/Keuangan/Pegawai-Keuangan/chart/page.tsx
@@ -1,11 +1,32 @@
 "use client";
 
 import React from "react";
+import { useState } from "react";
 import { Line } from "react-chartjs-2";
 import { Chart as ChartJS } from "chart.js/auto";
+import UserData from "@/data/keuangan/page";
 
-function LineChart({ chartData }: { chartData: any }) {
-  return <Line data={chartData} />;
+function LineChart() {
+  const [userData, setUserData] = useState({
+    labels: UserData.map((data) => data.month),
+    datasets: [
+      {
+        label: "Pegawai Tetap",
+        data: UserData.map((data) => data.PT),
+        tension: 0.3,
+        fill: true,
+        backgroundColor: "rgba(117, 194, 246, 0.8)",
+      },
+      {
+        label: "Pegawai Tidak Tetap",
+        data: UserData.map((data) => data.PTT),
+        tension: 0.3,
+        fill: true,
+        backgroundColor: "rgba(31, 128, 194, 0.9)",
+      },
+    ],
+  });
+  return <Line data={userData} />;
 }
 
 export default LineChart;
