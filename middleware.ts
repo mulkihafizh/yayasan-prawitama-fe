154afc5c771ece91dd237f671c93fc83e57c9e86
MulkiHafizh <ukiyo142@gmail.com>
2023-11-03 09:52:47 +0700

    fix undefined

--- middleware.ts
+++ middleware.ts
@@ -18,11 +18,7 @@ export function middleware(request: NextRequest) {
       }
     }
   }
-  if (
-    request.nextUrl.pathname === "/Dashboard-Guru" ||
-    request.nextUrl.pathname === "/Dashboard-Keuangan" ||
-    request.nextUrl.pathname === "/Dashboard-Kepegawaian"
-  ) {
+  if (request.nextUrl.pathname === "/Dashboard-Guru") {
     if (request.cookies.has("token")) {
       let role = request.cookies.get("role");
       if (role?.value === "payroll_admin") {
@@ -35,6 +31,38 @@ export function middleware(request: NextRequest) {
         );
       } else {
         return NextResponse.next();
+      }
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  if (request.nextUrl.pathname === "/Dashboard-Keuangan") {
+    if (request.cookies.has("token")) {
+      let role = request.cookies.get("role");
+      if (role?.value === "payroll_admin") {
+        return NextResponse.next();
+      } else if (role?.value === "employee_admin") {
+        return NextResponse.redirect(
+          new URL("/Dashboard-Kepegawaian", request.url)
+        );
+      } else {
+        return NextResponse.redirect(new URL("/Dashboard-Guru", request.url));
+      }
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  if (request.nextUrl.pathname === "/Dashboard-Kepegawaian") {
+    if (request.cookies.has("token")) {
+      let role = request.cookies.get("role");
+      if (role?.value === "payroll_admin") {
+        return NextResponse.redirect(
+          new URL("/Dashboard-Keuangan", request.url)
+        );
+      } else if (role?.value === "employee_admin") {
+        return NextResponse.next();
+      } else {
+        return NextResponse.redirect(new URL("/Dashboard-Guru", request.url));
       }
     } else {
       return NextResponse.redirect(new URL("/", request.url));
