9bb6fe7a86ecdc6b4659178b57b55e353dd2d06e
MulkiHafizh <ukiyo142@gmail.com>
2023-11-03 23:23:47 +0700

    data kepegawaian

--- app/components/Kepegawaian/Pegawai-Kepegawaian/page.tsx
+++ app/components/Kepegawaian/Pegawai-Kepegawaian/page.tsx
@@ -8,26 +8,7 @@ import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 Chart.register(CategoryScale);
 
-const ChartPage = () => {
-  const userData = {
-    labels: UserData.map((data) => data.month),
-    datasets: [
-      {
-        label: "Pegawai Tetap",
-        data: UserData.map((data) => data.PT),
-        tension: 0.3,
-        fill: true,
-        backgroundColor: "rgba(252, 214, 114, 0.8)",
-      },
-      {
-        label: "Pegawai Tidak Tetap",
-        data: UserData.map((data) => data.PTT),
-        tension: 0.3,
-        fill: true,
-        backgroundColor: "rgba(217, 157, 0, 0.9)",
-      },
-    ],
-  };
+const ChartPage = ({ data }: any) => {
   return (
     <div className={css.MainContainer}>
       <div className={css.title}>Jumlah Karyawan</div>
@@ -37,7 +18,19 @@ const ChartPage = () => {
         <div className={"max-md:!gap-3 " + css.presentase}>
           <div className={"max-md:!gap-2 " + css.box1}>
             <div className={css.number}>
-              <div className={"max-md:!text-[16px] " + css.h1}>115</div>
+              <div className={"max-md:!text-[16px] " + css.h1}>
+                {data != null &&
+                data != undefined &&
+                Object.keys(data).length > 0 ? (
+                  data.filter(
+                    (item: any) => item.employment_type == "Permanent"
+                  ).length
+                ) : (
+                  <span className="min-w-[60px]  text-sm animate-colorChange text-transparent  ml-2 rounded-2xl ">
+                    Name
+                  </span>
+                )}
+              </div>
             </div>
             <div className={"max-md:!text-[12px] " + css.kt}>
               Karyawan Tetap
@@ -45,7 +38,18 @@ const ChartPage = () => {
           </div>
           <div className={"max-md:!gap-2 " + css.box2}>
             <div className={css.number}>
-              <div className={"max-md:!text-[16px] " + css.h1}>96</div>
+              <div className={"max-md:!text-[16px] " + css.h1}>
+                {data != null &&
+                data != undefined &&
+                Object.keys(data).length > 0 ? (
+                  data.filter((item: any) => item.employment_type == "Contract")
+                    .length
+                ) : (
+                  <span className="min-w-[60px]  text-sm animate-colorChange text-transparent   rounded-2xl ">
+                    Name
+                  </span>
+                )}
+              </div>
             </div>
             <div className={"max-md:!text-[12px] " + css.kt}>
               Karyawan Tidak Tetap
@@ -53,7 +57,13 @@ const ChartPage = () => {
           </div>
         </div>
         <div className={css.chart}>
-          <LineChart chartData={userData} />
+          {data != null && data != undefined && Object.keys(data).length > 0 ? (
+            <LineChart chartData={data} />
+          ) : (
+            <span className="min-w-[60px]  text-xl animate-colorChange text-transparent   rounded-2xl ">
+              Name Placeholder
+            </span>
+          )}
         </div>
       </div>
     </div>
